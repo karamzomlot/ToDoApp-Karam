d49aa2d5573bb7607e6afb93652beb7e4ae63a4a
karam-zomlut <zomlutk@gmail.com>
2022-01-29 13:58:32 +0200

    Add Edit Form

--- js/main.js
+++ js/main.js
@@ -1,21 +1,46 @@
 // Open Form to Add Task
-let openBtn = document.querySelector('.add-sec .btn.open-btn');
-let addForm = document.querySelector(".add-sec .add-form");
+let openBtn = document.querySelector(".add-sec .sec-top .btn.open-btn");
+let addForm = document.querySelector(".add-sec .form.add-form");
 
-let slideDown = () => {
-    addForm.classList.toggle('open-form');
-    openBtn.classList.toggle('close');
-}
+openBtn.addEventListener("click", () => {
+    addForm.classList.toggle("open-form");
+    openBtn.classList.toggle("close");
+});
 
-openBtn.addEventListener('click', slideDown);
+// Open & Close Form to Edit Task
+let secTop = document.querySelector(".add-sec .sec-top");
+let closeEditBtn = document.querySelector(".add-sec .sec-top .btn.close-btn");
+let editForm = document.querySelector(".add-sec .form.edit-form");
+let tasksList = document.querySelector(".view-sec .tasks-list");
 
+tasksList.addEventListener("click", (e) => {
+    if (e.target.dataset.id == "edit"){
+        // Close Add Form If It Open
+        if (addForm.classList.contains("open-form")){
+            addForm.classList.remove("open-form");
+            openBtn.classList.remove("close");
+        }
+
+        // Open Edit Form
+        editForm.classList.add("open-form");
+        secTop.classList.add("edit-opened");
+
+        // Scroll To Top Page To Edit Data
+        window.scrollTo({top: 0})
+    }
+});
+
+closeEditBtn.addEventListener("click", () => {
+    editForm.classList.remove("open-form")
+    secTop.classList.remove("edit-opened");
+})
 
 // Get Task Info
 
-let taskTitle = document.getElementById('task-title');
-let taskDay = document.getElementById('task-day');
-let taskTime = document.getElementById('task-time');
-let saveBtn = document.getElementById('save-btn');
+let taskTitle = document.getElementById("task-title");
+let taskDay = document.getElementById("task-day");
+let taskTime = document.getElementById("task-time");
+let saveBtn = document.getElementById("save-btn");
 
 let getData = (e) => {
     e.preventDefault();
@@ -25,16 +50,16 @@ let getData = (e) => {
     console.log(taskTime.value)
 }
 
-saveBtn.addEventListener('click', getData);
+saveBtn.addEventListener("click", getData);
 
 
 // Done Task
-let singleTask = document.querySelector('.tasks-list .single-task');
-let doneTaskBtn = document.querySelector('.single-task .done-btn');
+let singleTask = document.querySelector(".tasks-list .single-task");
+let doneTaskBtn = document.querySelector(".single-task .done-btn");
 
 let doneTask = () => {
-    singleTask.classList.toggle('done-task');
+    singleTask.classList.toggle("done-task");
 }
 
 
-doneTaskBtn.addEventListener('click', doneTask)
+doneTaskBtn.addEventListener("click", doneTask)
